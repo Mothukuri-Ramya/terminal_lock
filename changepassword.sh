d832abc863b47914d251a05cf76c2104390cf35f
ramyamothukuri <83526124+ramyamothukuri@users.noreply.github.com>
2021-05-04 16:54:23 +0530

    Add files via upload

---
+++ changepassword.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+read -s -p "enter terminal password:" "password"
+echo
+sed "s/user/$password/g" password.sh > setlock
+rm password.sh
+sudo chmod +x setlock
+sudo mv setlock /usr/bin
+sudo echo setlock >> $HOME/.bashrc
+sudo rm -r ../terminal_lock 
