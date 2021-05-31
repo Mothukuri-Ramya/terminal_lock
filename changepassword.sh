24956f5c88de15d28c66057864efd5d464524fe2
ramyamothukuri <83526124+ramyamothukuri@users.noreply.github.com>
2021-05-31 11:03:53 +0530

    Update changepassword.sh

--- changepassword.sh
+++ changepassword.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-read -s -p "enter terminal password:" "password"
+read -s -p "enter terminal password:" password
 echo
-sed "s/user/$password/g" password.sh > setlock
+sed "s/admin/$password/g" password.sh > setlock
 rm password.sh
 sudo chmod +x setlock
 sudo mv setlock /usr/bin
 sudo echo setlock >> $HOME/.bashrc
-sudo rm -r setlock
+cd ..
+sudo rm -r terminal_lock
